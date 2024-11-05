aa03630990730b6ebd7665f6d48d8c9f37ccd1f8
Abaakil <154565344+Abaakil35@users.noreply.github.com>
2024-11-05 23:45:05 +0100

    on the way!

--- Script.js
+++ Script.js
@@ -1,17 +1,26 @@
-// Pour L'Affichage Dans la Weather-Info :
+// For Seeing The Weather Info  :
 const TempActuel = document.getElementById("degree");
-const TempMax = document.getElementById("max");
-const TempMin = document.getElementById("min");
-const Humidite = document.getElementById("humidity");
-const CountryName = document.getElementById("country-name");
+const CurrentCity = document.getElementById("City-name")
+const CountryName = document.getElementById("Country-name");
 const WeatherIcon = document.getElementById("svg-current");
+const DateTime =document.getElementById("current-datetime");
 
-//Pour L'input de Données  :
-const InputSearch = document.getElementBy("input");
+//For Input of Data :
+const InputSearch = document.getElementById("input");
 const SearchButton = document.getElementById("search-btn");
 
-//Pour L'Afiichage  de la Recherche :
+//For Seeing The Result Of Fetching  Data :
+const  StatusWeather = document.getElementById("status");
+const TempMax = document.getElementById("max-temp");
+const TempMin = document.getElementById("min-temp");
+const Humidite = document.getElementById("humidity");
+const WindSpeed = document.getElementById("wind");
+const Cloud = document.getElementById("cloudy");
 
-
+//For Daily Forecast 7 days:
+const statusIcon  = document.getElementById("svg-status");
+const day = document.getElementsByClassName("time-2");
+const statusDay = document.getElementsByClassName("status-2");
+const  tempDay = document.getElementsByClassName("temp-2");
 
 
