347089db89f12ddf92cd1507667361558df6e4f4
Abaakil <154565344+Abaakil35@users.noreply.github.com>
2024-11-05 22:58:18 +0100

    on the way!

---
+++ Script.js
@@ -0,0 +1,17 @@
+// Pour L'Affichage Dans la Weather-Info :
+const TempActuel = document.getElementById("degree");
+const TempMax = document.getElementById("max");
+const TempMin = document.getElementById("min");
+const Humidite = document.getElementById("humidity");
+const CountryName = document.getElementById("country-name");
+const WeatherIcon = document.getElementById("svg-current");
+
+//Pour L'input de Données  :
+const InputSearch = document.getElementBy("input");
+const SearchButton = document.getElementById("search-btn");
+
+//Pour L'Afiichage  de la Recherche :
+
+
+
+
